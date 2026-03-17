7cc980ec9976891228e4c1197d2b5dd5751914ea
Peter Freedman <frdmanp@gmail.com>
2026-03-17 10:46:21 -0400

    working on the build command. Hopefully this fixes it on mac.

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
-pyinstaller            \
- --onedir --windowed   \
- --noconfirm --clean   \
- --collect-all PyQt6   \
- --collect-all PyQt6.QtWebEngineWidgets \
- TWMA.py
+pyinstaller \
+  --onedir --windowed \
+  --noconfirm --clean \
+  --add-data="src/static/js:./static/js" \
+  --add-data="src/static:./static" \
+  --add-data="src/templates:./templates" \
+  --collect-all PyQt6 \
+  --collect-all PyQt6.QtWebEngineWidgets \
+  TWMA.py

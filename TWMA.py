30cc0b0572c8b511cd73062f8fb70d56f1849f2f
Peter Freedman <frdmanp@gmail.com>
2026-03-26 17:47:03 -0400

    SQLAlchemy Database Schema Verified Redid the database design using SQLAlchemy for futureproofing.
    - Added a travel field in preparation to support travel fees in the intake form
    - Checked that it creates a schema as expected using a sqlite viewer
    - Checked that the location the db is created in hasn't changed.
    
    TODO: Find a way to turn the form into a series of inserts

--- TWMA.py
+++ TWMA.py
@@ -2,27 +2,31 @@ from threading import Thread
 import os as os
 from pathlib import Path
 
-from src.core import app_core, parse_form, app, db
-from src.db import *
+from src.core import app_core, parse_form, app
+from src.db import db
 from src.forms import Consent
 from src.view import init_frontend
 
 
-def config_app():
+def setup_db():
+    os.makedirs(os.path.join(os.path.expanduser('~'), 'Documents', 'TWMA_DB'), exist_ok=True)
     db_path = str(Path(os.path.join(os.path.expanduser('~'), 'Documents', 'TWMA_DB')))
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}/TWMA.db"
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
 
 
 def process_form():
     while form := app_core.dequeue():
         [client, patient, appt, vet] = parse_form(form)
         Consent(client, patient, appt).generate()
-        Database().add_record(client, patient, appt, vet)        
+        # TODO: inserting needs to change now. 
+        # Database().add_record(client, patient, appt, vet)        
     
 if __name__ == '__main__':
     backend = Thread(target=process_form)
     backend.start()
-    config_app()
+    setup_db()
     init_frontend()  
     backend.join()
--- src/core.py
+++ src/core.py
@@ -1,23 +1,14 @@
 from queue import Queue, ShutDown
 
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy.orm import DeclarativeBase
 
-from src.db import Client, Patient, Appointment, Vet
 from src.utils import address, wrap_path #move this if parse_form ever finds a new home
-
-
-class Base (DeclarativeBase):
-    pass
 
 app = Flask(
     __name__,
     template_folder=wrap_path("src/templates"),
     static_folder=wrap_path("src/static"),
 )
-
-db = SQLAlchemy(model_class=Base)
 
 class Core():
    """DO NOT create a new instance, use the provided app_core instead
@@ -48,13 +39,15 @@ class Core():
 
 app_core = Core()
 
-def parse_form(form: dict[str, str]) -> tuple[Client, Patient, Appointment, Vet]:
-    return tuple(
-        [
-            Client(form['client'], form['email'], form['phone'], address(form['address'], form['city'], form['state'], form['zip'])),
-            Patient(form['patient'], form['animal'], form['sex'], form['weight'], form['disposal'], form['prints'], form['age'], form['breed'], form['color'], form['notes']),
-            Appointment(form['date'], form['time']),
-            Vet(form['vet'])
-        ]
-    )
+# TODO: redo form parsing. 
+def parse_form(form: dict[str, str]):
+   pass
+#     return tuple(
+#         [
+#             Client(form['client'], form['email'], form['phone'], address(form['address'], form['city'], form['state'], form['zip'])),
+#             Patient(form['patient'], form['animal'], form['sex'], form['weight'], form['disposal'], form['prints'], form['age'], form['breed'], form['color'], form['notes']),
+#             Appointment(form['date'], form['time']),
+#             Vet(form['vet'])
+#         ]
+#     )
 

853ef38c18b290b00d1860ca847684dede20472f
Peter Freedman <frdmanp@gmail.com>
2026-04-14 17:50:44 -0400

    finally done with consent form updates

---
+++ genpdf.py
@@ -0,0 +1,29 @@
+from src.forms import FormFactory, FormType
+import subprocess
+
+data = {
+    "date": "test",
+    "client": "Peter Freedman",
+    "address": "307 Winding Hill Drive",
+    "city": "Lancaster",
+    "state": "PA",
+    "zip": "17601",
+    "phone": "717-799-6491",
+    "patient": "Sam",
+    "animal": "Dog",
+    "breed": "Husky Mix",
+    "sex": "Male",
+    "age": "14",
+    "weight": "40",
+    "email": "frdmanp@gmail.com",
+    "vet": "Mark Huber",
+    "date": "2024-10-10",
+    "time": "15:50",
+    "disposal": "home",
+    "travel": "55"
+}
+
+f = FormFactory(data)
+con = f.generate(FormType.CONSENT)
+con.save("./")
+subprocess.run(["firefox", "2024-10-10/Freedman.pdf"])

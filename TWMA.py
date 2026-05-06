b9b55f968339501ffa18668c3f5ed19c8b83a407
Peter Freedman <frdmanp@gmail.com>
2026-05-05 20:57:03 -0400

    feat: settings added support for settings. changed all instances of most filepaths throughout the codebase to be based on the filepaths in the settings object, which is managed by the core. the filepaths that remain are those used for blank forms, because they are static and never controlled by the end user.

--- TWMA.py
+++ TWMA.py
@@ -1,6 +1,5 @@
 from threading import Thread
 import os as os
-from pathlib import Path
 
 from src.core import app_core, app
 from src.db import *
@@ -9,9 +8,8 @@ from src.view import init_frontend
 
 
 def setup_db():
-    os.makedirs(os.path.join(os.path.expanduser('~'), 'Documents', 'TWMA_DB'), exist_ok=True)
-    db_path = str(Path(os.path.join(os.path.expanduser('~'), 'Documents', 'TWMA_DB')))
-    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}/TWMA.db"
+    os.makedirs(app_core.settings.db_path, exist_ok=True)
+    app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{app_core.settings.db_path}/TWMA.db"
     db.init_app(app)
     with app.app_context():
         db.create_all()
@@ -19,12 +17,15 @@ def setup_db():
 
 def process_form():
     while data := app_core.dequeue():
-        fac = FormFactory(data)
-        con = fac.generate(FormType.CONSENT)
-        con.save()
-        bom = fac.generate(FormType.BILL_OF_MATERIALS)
-        bom.save()
-        register_pt(data)
+        if (data['form_id'] == 'settings'):
+            app_core.settings.update_settings(data)
+        else:
+            fac = FormFactory(data)
+            con = fac.generate(FormType.CONSENT)
+            con.save()
+            bom = fac.generate(FormType.BILL_OF_MATERIALS)
+            bom.save()
+            register_pt(data)
     
 if __name__ == '__main__':
     try:
--- src/settings.py
+++ src/settings.py
@@ -3,6 +3,7 @@ from json import dumps, load
 from io import open
 from pathlib import Path
 from typing import Any
+from shutil import copytree, rmtree
 
 from src.utils import wrap_path 
 
@@ -11,10 +12,10 @@ class Settings:
 
     def __init__ (self):
         self.db_path = str(path.join(path.expanduser('~'), 'Documents', 'DB'))
-        self.out_path = str(path.join(path.expanduser('~'), 'Desktop'))
-        self.log_path = str(path.join(wrap_path("logs", src_level=True))) 
+        self.out_path = str(path.join(path.expanduser('~'), 'Desktop', 'TWMA Files'))
+        self._log_path = str(path.join(wrap_path("logs", src_level=True))) 
         self._settings_path = path.join(wrap_path("settings", src_level=True), "opt.conf")
-        self.version = 0.1
+        self._version = 0.1
 
         self._read_settings()
     
@@ -28,7 +29,7 @@ class Settings:
          self.unpack_json()
 
     def save_settings(self):
-       file = open(self._settings_path, mode='x')
+       file = open(self._settings_path, mode='w')
        file.write(self.pack_json())
        file.close()
 
@@ -45,6 +46,32 @@ class Settings:
        for [k, _] in vars(self).items():
           self.__setattr__(k, json[k])
 
+    def check_filepaths (self, data: dict[str, Any]) -> list[str]:
+       bad_fps = []
+       for [opt, _] in vars(self).items():
+          if opt[0] == '_':
+             continue
+          if not path.exists(data[opt]):
+             bad_fps.append(data[opt])
+       return bad_fps
+
+    def is_public_setting (self, setting: str):
+       return setting[0] != '_'
+    
     def update_settings (self, data: dict[str, Any]):
-       #TODO: when there's a settings form, this will take that form (data) and update fields accordingly.
-       pass       
+      # not allowed to make fps and some don't exist already
+      if (bad_fps := self.check_filepaths(data)) and not data['create_fps']:
+         return f'The following filepaths did not exist and cannot be created:/n{'/n'.join(bad_fps)}'
+      
+      for [opt, fp] in vars(self).items():
+         try:
+            if data[opt] and self.is_public_setting(opt):
+               copytree(fp, data[opt])
+               if (data['remove_old']):
+                  rmtree(fp)
+               vars(self).update({opt : data[opt]})
+         except Exception as e:
+            return str(e)
+         
+         self.save_settings()
+         return 'success'
